208f79515f787c653f20d4e0b386196f5de95546
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2021-09-16 12:16:17 +0000

    Sync LeetCode submission - Max Area of Island (cpp)

--- problems/max_area_of_island/solution.cpp
+++ problems/max_area_of_island/solution.cpp
@@ -1,35 +1,27 @@
-int MAX= 0 ;
-int cur ;
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        MAX = 0;
+        int MAX = 0;
         for(int i = 0 ; i < grid.size() ; i++){
             for(int j = 0 ; j < grid[i].size() ; j++){
-                cur = 0;
-                dfs(grid,i,j);
-                MAX = max(MAX,cur);
+                MAX = max(MAX,dfs(grid,i,j));
             }
         }
         
         return MAX;
     }
     
-    void dfs(vector<vector<int>>& grid, int i , int j){
-        if( grid[i][j] == 0 ) return ;
+    int dfs(vector<vector<int>>& grid, int i , int j){
+        if( grid[i][j] == 0 ) return 0 ;
         grid[i][j] = 0;
-        cur++;
+        int cur = 1;
+        vector<pair<int,int>> v(4);
+        v  = {{i+1,j},{i-1,j},{i,j+1},{i,j-1}};
         
-        if(i+1 < grid.size())
-            dfs(grid,i+1,j);
+        for(auto x:v)
+            if(x.first >= 0 && x.first < grid.size() && x.second >= 0 && x.second < grid[0].size())
+                cur += dfs(grid,x.first,x.second);
+        return cur;
         
-        if(i-1 >= 0)
-            dfs(grid,i-1,j);
-        
-        if(j+1 < grid[i].size())
-            dfs(grid,i,j+1);
-        
-        if(j-1 >= 0)
-            dfs(grid,i,j-1);
     }
 };
