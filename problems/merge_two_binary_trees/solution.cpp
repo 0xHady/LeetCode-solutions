9a7dbbd9427b430ea9cc61206b7f524500294ab2
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2021-09-16 13:21:20 +0000

    Sync LeetCode submission - Merge Two Binary Trees (cpp)

--- problems/merge_two_binary_trees/solution.cpp
+++ problems/merge_two_binary_trees/solution.cpp
@@ -12,16 +12,13 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        return dfs(root1,root2);
-    }
-    TreeNode* dfs(TreeNode* root1, TreeNode* root2){
         if(root1 == NULL && root2 == NULL) return NULL;
         if(root1 != NULL && root2 == NULL) return root1;
         if(root1 == NULL && root2 != NULL) return root2;
         
         TreeNode *merge = new TreeNode(root1->val + root2 ->val);
-        merge->right = dfs(root1->right ,root2->right);
-        merge->left  = dfs(root1->left  ,root2->left );
+        merge->right = mergeTrees(root1->right ,root2->right);
+        merge->left  = mergeTrees(root1->left  ,root2->left );
         
         return merge;
     }
