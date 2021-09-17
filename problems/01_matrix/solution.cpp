4c08d2082edb40701bb4a042190d6117495f966d
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2021-09-17 08:49:22 +0000

    Sync LeetCode submission - 01 Matrix (cpp)

---
+++ problems/01_matrix/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int,int>> q;
+        
+        for(int i= 0; i < mat.size() ; i++)
+            for(int j = 0 ; j < mat[0].size() ; j++)
+                if(mat[i][j] == 0)
+                    q.push({i,j});
+                else
+                    mat[i][j] = -1;
+        
+        vector<pair<int,int>> direc(4);
+        direc = {{0,1},{0,-1},{1,0},{-1,0}};
+        int depth = 0;
+        while(!q.empty()){
+            int size = q.size();
+            depth++;
+            while(size--){
+                pair<int,int> cell;
+                cell = q.front();
+                q.pop();
+                for(auto i:direc){
+                    int x = cell.first+i.first;
+                    int y = cell.second+i.second;
+                    if(x < mat.size() && x >= 0 && y >= 0 && y < mat[0].size() && mat[x][y] == -1){
+                        mat[x][y] = depth;
+                        q.push({x,y});
+                    }
+                }
+            }
+        }
+        return mat;
+    }
+    
+};
